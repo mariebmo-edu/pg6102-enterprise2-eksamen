099587b4c3a6dc1474c687520a350d73c4993a1e
mariestigen <marie.stigen@hotmail.com>
2022-11-24 19:53:10 +0100

    Fixed test environment - mockMvc doesn't work

--- order-service/src/test/kotlin/snr/student1012/orderservice/integrationtests/OrderApiTest.kt
+++ order-service/src/test/kotlin/snr/student1012/orderservice/integrationtests/OrderApiTest.kt
@@ -2,7 +2,6 @@ package snr.student1012.orderservice.integrationtests
 
 import org.json.JSONObject
 import org.junit.jupiter.api.MethodOrderer
-import org.junit.jupiter.api.Order
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestMethodOrder
 import org.springframework.beans.factory.annotation.Autowired
@@ -14,25 +13,25 @@ import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.post
 
-@SpringBootTest
+
+@SpringBootTest()
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@ActiveProfiles("test")
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 @TestMethodOrder(MethodOrderer.OrderAnnotation::class)
 class OrderApiTest(@Autowired private val mockMvc: MockMvc) {
 
     val baseUrl = "http://localhost:8080/api/order"
 
     @Test
-    @Order(1)
     fun shouldCreateOrder() {
         val orderPayLoad = JSONObject()
-         orderPayLoad.put("userId", 2)
-            orderPayLoad.put("status", "WAITING")
-            orderPayLoad.put("paymentStatus", "WAITING")
-            orderPayLoad.put("totalPrice", 200.0)
-            orderPayLoad.put("currency", "NOK")
-            orderPayLoad.put("description", "cat")
+        orderPayLoad.put("userId", 2)
+        orderPayLoad.put("status", "WAITING")
+        orderPayLoad.put("paymentStatus", "WAITING")
+        orderPayLoad.put("totalPrice", 200.0)
+        orderPayLoad.put("currency", "NOK")
+        orderPayLoad.put("description", "cat")
 
         mockMvc.post(baseUrl){
             contentType = MediaType.APPLICATION_JSON
